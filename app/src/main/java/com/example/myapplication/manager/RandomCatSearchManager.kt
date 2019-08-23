ba33e050852873b5abb15b5614a69e28c10b7607
pcholt <pcholt@gmail.com>
2019-08-23 11:39:50 +1000

    minor

--- app/src/main/java/com/example/myapplication/manager/RandomCatSearchManager.kt
+++ app/src/main/java/com/example/myapplication/manager/RandomCatSearchManager.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.manager
 import com.example.myapplication.AnimalSearchManager
 import com.example.myapplication.net.RandomCat
 
-class RandomCatSearchManager(val randomCat: RandomCat) : AnimalSearchManager {
+class RandomCatSearchManager(private val randomCat: RandomCat) : AnimalSearchManager {
     override suspend fun search() =
         randomCat.randomCat().file
 
